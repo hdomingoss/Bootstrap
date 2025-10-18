44704b946e7b27abfc75a2c828edfd0104c45c67
hdomingoss <higuinho.domingos@gmail.com>
2025-10-17 21:30:02 -0300

    Add files via upload

---
+++ Aula 17.10 Java/js/meuscript.js
@@ -0,0 +1,8 @@
+function exibir(){
+   let paragrafo = document.getElementById('nome');
+   console.log(paragrafo);
+   paragrafo.innerHTML= "SEXTOUUU!!🔥"
+}
+function ocultar(){
+  document.getElementById("nome").innerHTML= '';
+}
